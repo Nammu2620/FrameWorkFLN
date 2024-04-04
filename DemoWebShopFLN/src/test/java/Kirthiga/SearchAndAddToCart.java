33810311f1001452bb806e62ac071d3e381f678c
kirth <kirth@Kirthiga>
2024-04-04 15:37:35 +0530

    KIRTHIGA

--- DemoWebShopFLN/src/test/java/Kirthiga/SearchAndAddToCart.java
+++ DemoWebShopFLN/src/test/java/Kirthiga/SearchAndAddToCart.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 
 import Element_Repo.Home_Page;
 import Element_Repo.LoginPage;
+import Element_Repo.ShoppingCart_Page;
 import Generic_Library.BaseClass;
 
 public class SearchAndAddToCart extends BaseClass {
@@ -18,29 +19,24 @@ public class SearchAndAddToCart extends BaseClass {
 		lp.Login_Actions("kirthigagnanasekar2001@gmail.com","Keerthika@2505");
 		
 		//searching product 
-		WebElement searchBar = driver.findElement(By.id("small-searchterms"));
-		searchBar.sendKeys("laptop");
+		lp.getSearchBar().sendKeys("laptop");
 		lp.getSearchButton().click();
 		
 		//adding product to cart
-		WebElement laptop = driver.findElement(By.xpath("//a[text()='14.1-inch Laptop']/../..//input[@value='Add to cart']"));
-		laptop.click();
+		ShoppingCart_Page sc=new ShoppingCart_Page(driver);
+		sc.getLap_AddToCart().click();
 		
 		//To view shopping cart
 		lp.getShoppingCartlink().click();
 		
 		//To change quantity in shopping cart
-		WebElement quantity = driver.findElement(By.xpath("//a[text()='14.1-inch Laptop']/../..//input[@class='qty-input']"));
-		quantity.clear();
-		quantity.sendKeys("2");
+		sc.getQuantityTB().clear();
+		sc.getQuantityTB().sendKeys("2");
 		
 		//To Remove product from cart
-		WebElement removeLaptop = driver.findElement(By.xpath("//a[text()='14.1-inch Laptop']/../..//input[@name=\"removefromcart\"]"));
-		removeLaptop.click();
-		WebElement updateCartButton = driver.findElement(By.name("updatecart"));
-		updateCartButton.click();
+		sc.getRemoveLap().click();
+		sc.getUpdateCartButton().click();
 		
-	
 		
 	}
 
